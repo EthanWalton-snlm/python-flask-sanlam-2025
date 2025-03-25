0238e1a6d013b1ee0cdb3a94abcb6828d77086e1
EthanWalton-snlm <ethan.walton@sanlam.co.za>
2025-03-25 10:50:23 +0200

    Day 24

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hey flask")
+from flask import Flask
+
+from routes.movies_bp import movies_bp
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def hello_world():
+    return "<h2>hey, flask!!</h2>"
+
+
+# Flask - Blueprints
+app.register_blueprint(movies_bp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
