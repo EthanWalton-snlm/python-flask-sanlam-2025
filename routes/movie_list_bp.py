af7cd9637b7f814b355eb2a82f2c945de6b0dcb3
EthanWalton-snlm <ethan.walton@sanlam.co.za>
2025-03-28 15:42:55 +0200

    Day 27 - Bootstrap

--- routes/movie_list_bp.py
+++ routes/movie_list_bp.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 
+from constants import STATUS_CODE
+from extensions import db
 from models.movies import Movie
 
 movie_list_bp = Blueprint("movie_list_bp", __name__)
@@ -23,3 +25,33 @@ def movie_details_page(id):
         return render_template("not-found.html")
 
     return render_template("movie-details.html", movie=movie.to_dict())
+
+
+@movie_list_bp.get("/new")
+def add_movie():
+    return render_template("add-movie.html")
+
+
+@movie_list_bp.post("/")
+def create_movie():
+    data = {
+        "name": request.form.get("name"),
+        "poster": request.form.get("poster"),
+        "rating": request.form.get("rating"),
+        "summary": request.form.get("summary"),
+        "trailer": request.form.get("trailer"),
+    }
+    new_movie = Movie(**data)
+
+    try:
+        db.session.add(new_movie)
+        db.session.commit()
+
+        # return {
+        #     "message": "Movie created successfully",
+        #     "data": new_movie.to_dict(),
+        # }, STATUS_CODE["CREATED"]
+        return redirect(url_for("movie_list_bp.movies_page"))
+    except Exception as e:
+        db.session.rollback()  # restores data, cannot be done after commit()
+        return redirect(url_for("movie_list_bp.add_movie"))
